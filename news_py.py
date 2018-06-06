0e7733e5326b8742fa88a858a847e8fc02397dc6
Djeyla <38537225+Djeyla@users.noreply.github.com>
2018-06-06 22:24:21 +0300

    Add files via upload

---
+++ news_py.py
@@ -0,0 +1,52 @@
+import chardet
+
+
+def find_words():
+    words = {}
+    for i in f:
+        i = i.split()
+        for x in i:
+            if len(x) > 6:
+                k = i.count(x)
+                words[x] = k
+    words = sorted(words.items(), key=lambda x: x[1], reverse=True)
+
+    q = 1
+    for z in words:
+        if q > 5:
+            break
+        q = q + 1
+        print(str(z))
+
+
+print('Результаты файла newsafr')
+
+with open('newsafr.txt', 'r', encoding='utf-8') as f:
+    find_words()
+
+print('Результаты файла newscy')
+with open('newscy.txt', 'rb') as f:
+    data = f.read()
+    result = chardet.detect(data)
+    print(result)
+
+with open('newscy.txt', 'r', encoding='KOI8-R') as f:
+    find_words()
+
+print('Результаты файла newsfr')
+with open('newsfr.txt', 'rb') as f:
+    data = f.read()
+    result = chardet.detect(data)
+    print(result)
+
+with open('newsfr.txt', 'r', encoding='ISO-8859-5') as f:
+    find_words()
+
+print('Результаты файла newsit')
+with open('newsit.txt', 'rb') as f:
+    data = f.read()
+    result = chardet.detect(data)
+    print(result)
+
+with open('newsit.txt', 'r', encoding='windows-1251') as f:
+    find_words()
